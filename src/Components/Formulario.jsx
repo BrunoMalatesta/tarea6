d677f3fe9c8de6eee92d11bceacf7bb27f7c12ab
Bruno Malatesta <6malacabeza9@gmail.com>
2023-11-22 15:27:53 -0300

    nuevocommit

---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import './RegistroForm.css';
+
+const RegistroForm = () => {
+  const [formData, setFormData] = useState({
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí podrías agregar la lógica para manejar el envío del formulario
+    // y la validación de los datos.
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nombre:
+        <input
+          type="text"
+          name="nombre"
+          value={formData.nombre}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Apellido:
+        <input
+          type="text"
+          name="apellido"
+          value={formData.apellido}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Teléfono:
+        <input
+          type="tel"
+          name="telefono"
+          value={formData.telefono}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Confirmar Password:
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Registrarse</button>
+    </form>
+  );
+};
+
+export default RegistroForm;
